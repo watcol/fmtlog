a3aaba5944af6fb52c11697a77467357cecfaa73
watcol <potfman@gmail.com>
2021-01-07 22:34:04 +0900

    Imroved writing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 //! A simple configurable logger with format specification.
 extern crate log;
+extern crate thread_local;
 
 use log::*;
+use thread_local::ThreadLocal;
+use std::cell::RefCell;
 use std::{fmt, fs};
-use std::io::Write;
+use std::io::{
+    self, Write
+};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Output {
@@ -32,21 +37,64 @@ impl Default for Output {
     }
 }
 
-/// The body of fmtlog.
-#[derive(Default)]
-pub struct Logger {
-    output: Output,
+#[derive(Debug)]
+enum Stream {
+    Stdout(io::Stdout),
+    Stderr(io::Stderr),
+    File(fs::File)
 }
 
-impl Logger {
+impl Write for Stream {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self {
+            Stream::Stdout(w) => w.write(buf),
+            Stream::Stderr(w) => w.write(buf),
+            Stream::File(w) => w.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        match self {
+            Stream::Stdout(w) => w.flush(),
+            Stream::Stderr(w) => w.flush(),
+            Stream::File(w) => w.flush(),
+        }
+    }
+}
+
+/// The logger settings.
+#[derive(Default, Clone, Debug)]
+pub struct Config {
+    output: Output
+}
+
+impl Config {
     /// Create a new instance.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Set the output stream.
     pub fn output(&mut self, output: Output) -> &mut Self {
         self.output = output;
         self
+    }
+}
+
+/// The body of fmtlog.
+pub struct Logger {
+    config: Config,
+    writer: ThreadLocal<RefCell<Stream>>,
+}
+
+impl Logger {
+    /// Create a new instance.
+    pub fn new(config: Config) -> io::Result<Logger> {
+
+        Ok(Logger {
+            config,
+            writer: ThreadLocal::new(),
+        })
     }
 
     /// Set this logger active.
@@ -62,16 +110,19 @@ impl Log for Logger {
     }
 
     fn log(&self, record: &Record) {
-        match self.output.clone() {
-            Output::Stdout => println!("{}: {}", record.level(), record.args()),
-            Output::Stderr => eprintln!("{}: {}", record.level(), record.args()),
-            Output::File(path) => {
-                let mut file = fs::File::open(path)
-                    .expect("Failed to open the log file.");
-                write!(file, "{}: {}", record.level(), record.args())
-                    .expect("Failed to write the file.");
-            }
-        }
+        let writer = self.writer.get_or(|| {
+            let tmp = match self.config.output.clone() {
+                Output::Stdout => Stream::Stdout(io::stdout()),
+                Output::Stderr => Stream::Stderr(io::stderr()),
+                Output::File(path) => Stream::File(fs::File::open(path)
+                                                   .expect("Failed to read the file"))
+            };
+            RefCell::new(tmp)
+        });
+
+        let mut writer = writer.borrow_mut();
+        write!(writer, "{}: {}", record.level(), record.args())
+            .expect("Failed to write.");
     }
 
     fn flush(&self) {}
