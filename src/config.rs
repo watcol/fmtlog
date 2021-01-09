c89ce0852a9adbd3ba544d5844a2e02f97b02dd3
watcol <potfman@gmail.com>
2021-01-09 18:56:11 +0900

    Improved the document.

--- src/config.rs
+++ src/config.rs
@@ -34,36 +34,48 @@ impl Config {
     }
 
     /// Create a new instance from TOML.
+    ///
+    /// **Requires `conf-toml` feature.**
     #[cfg(feature = "conf-toml")]
     pub fn from_toml<T: AsRef<str>>(s: T) -> Result<Self, toml::de::Error> {
         toml::from_str(s.as_ref())
     }
 
     /// Output TOML from the configuration.
+    ///
+    /// **Requires `conf-toml` feature.**
     #[cfg(feature = "conf-toml")]
     pub fn to_toml(&self) -> Result<String, toml::ser::Error> {
         toml::to_string(self)
     }
 
     /// Create a new instance from JSON.
+    ///
+    /// **Requires `conf-json` feature.**
     #[cfg(feature = "conf-json")]
     pub fn from_json<T: AsRef<str>>(s: T) -> serde_json::Result<Self> {
         serde_json::from_str(s.as_ref())
     }
 
     /// Output JSON from the configuration.
+    ///
+    /// **Requires `conf-json` feature.**
     #[cfg(feature = "conf-json")]
     pub fn to_json(&self) -> serde_json::Result<String> {
         serde_json::to_string(self)
     }
 
     /// Create a new instance from YAML.
+    ///
+    /// **Requires `conf-yaml` feature.**
     #[cfg(feature = "conf-yaml")]
     pub fn from_yaml<T: AsRef<str>>(s: T) -> serde_yaml::Result<Self> {
         serde_yaml::from_str(s.as_ref())
     }
 
     /// Output YAML from the configuration.
+    ///
+    /// **Requires `conf-yaml` feature.**
     #[cfg(feature = "conf-yaml")]
     pub fn to_yaml(&self) -> serde_yaml::Result<String> {
         serde_yaml::to_string(self)
