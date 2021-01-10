8cb4634c6287e4af4a55b9ad0da6e695e800c25f
watcol <potfman@gmail.com>
2021-01-10 17:29:23 +0900

    Improved document

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,54 @@
 //! A simple configurable logger with format specification.
+//!
+//! ## Text-base Configuration
+//! This crate supports configuration by JSON, YAML, and TOML.
+//! - [JSON](https://en.wikipedia.org/wiki/JSON) (Requires feature `conf-json`)
+//! ```json
+//! {
+//!     "colorize": "auto",
+//!     "level": "info",
+//!     "output": {
+//!         "stream": "file",
+//!         "path": "log.txt"
+//!     }
+//! }
+//! ```
+//!
+//! - [YAML](https://en.wikipedia.org/wiki/YAML) (Requires feature `conf-yaml`)
+//! ```yaml
+//! colorize: auto
+//! level: info
+//! output:
+//!   stream: file
+//!   path: log.txt
+//! ```
+//!
+//! - [TOML](https://en.wikipedia.org/wiki/TOML) (Requires feature `conf-toml`)
+//! ```toml
+//! colorize = "auto"
+//! level = "info"
+//!
+//! [output]
+//! stream = "file"
+//! path = "log.txt"
+//! ```
+//!
+//! Available key and value are there.
+//!
+//! ### Data Format
+//! | Key | Value | Description |
+//! |-----|-------|-------------|
+//! | `colorize` | `on`, `auto`, `off` | Colorize the log if this value is `on` |
+//! | `level` | `off`, `error`, `warn`, `info`, `debug`, `trace` | Specify the log level. See [this](https://docs.rs/log) for the information. |
+//! | `output` | The following format | Specify the log destination. |
+//!
+//! - The `output` format
+//!
+//! | Key | Value | Description |
+//! |-----|-------|-------------|
+//! | `stream` | `stdout`, `stderr`, `file` | The output stream. |
+//! | `path` | Valid file path | Specify the file path when `stream` is `file`. |
+//!
 pub mod config;
 mod logger;
 mod stream;
