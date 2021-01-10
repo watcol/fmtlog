9c25f918d98b1e87c4d9d84446617506e0c9a84d
watcol <potfman@gmail.com>
2021-01-10 16:32:28 +0900

    Add functions from_*.

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,18 @@ pub fn default() -> Logger {
 pub fn new(config: Config) -> Logger {
     Logger::new(config)
 }
+
+#[cfg(feature = "conf-json")]
+pub fn from_json<T: AsRef<str>>(s: T) -> serde_json::Result<Logger> {
+    Ok(Logger::new(Config::from_json(s)?))
+}
+
+#[cfg(feature = "conf-yaml")]
+pub fn from_yaml<T: AsRef<str>>(s: T) -> serde_yaml::Result<Logger> {
+    Ok(Logger::new(Config::from_yaml(s)?))
+}
+
+#[cfg(feature = "conf-toml")]
+pub fn from_toml<T: AsRef<str>>(s: T) -> Result<Logger, toml::de::Error> {
+    Ok(Logger::new(Config::from_toml(s)?))
+}
