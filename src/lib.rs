30dc324c365b75c54c459c176d108ebdf83bdca7
watcol <potfman@gmail.com>
2021-01-15 22:31:03 +0900

    Add documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,26 @@
 //! | `%U` | | Enable underline. |
 //! | `%u` | | Disable underline. |
 //!
+//! ### Supported Color
+//! All supported color used by `%C` and `%O` is here.
+//! - `black`
+//! - `red`
+//! - `green`
+//! - `yellow`
+//! - `blue`
+//! - `magenta`
+//! - `cyan`
+//! - `white`
+//! - `bright_black`
+//! - `bright_red`
+//! - `bright_green`
+//! - `bright_yellow`
+//! - `bright_blue`
+//! - `bright_magenta`
+//! - `bright_cyan`
+//! - `bright_white`
+//! - `#ffffff` (Hexadecimal RGB)
+//!
 extern crate colored;
 extern crate log;
 extern crate thread_local;
